3368ad408a6f606b0067e6215a15f77dc7d9335f
Jakob Möller <jmoller@redhat.com>
2023-10-31 17:26:02 +0100

    more detailed notes and adjusted configuration

--- apply.sh
+++ apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 oc apply -f manifest_master-ipsec-systemd.yaml \
-    -f manifest_worker-ipsec-systemd.yaml \
     -f manifest_master-rt-kernel.yaml \
-    -f manifest_worker-rt-kernel.yaml
+    -f tuned-rt.yaml \
+    -f performance-profile.yaml
--- iperf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Used https://github.com/userdocs/iperf3-static to build iperf3 statically
-#
-#rpm2cpio iperf3-3.1.3-1.fc24.x86_64.rpm | cpio -D ./iperf/ -idmv
-butane iperf.bu -o iperf.yaml -d ./iperf
